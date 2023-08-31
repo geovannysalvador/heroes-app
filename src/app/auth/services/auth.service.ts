46cd163f98e4b01a017c7b19f076af0cf7a17abb
GeovannyTS <yowtecum@gmail.com>
2023-08-31 12:57:46 -0600

    Implementacion de Guard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+import { Observable, catchError, map, of, tap } from 'rxjs';
+
 import { enviroments } from 'src/environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+
 
 @Injectable({providedIn: 'root'})
 
@@ -26,6 +29,23 @@ export class AuthService {
     )
 
   }
+
+  checkAuth():Observable<boolean>{
+    if(!localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+    .pipe(
+        tap( user => this.user = user),
+        map( user => !!user),
+        catchError( err => of(false)),
+    )
+
+
+  }
+
+
   logout(){
     this.user = undefined;
     localStorage.clear();
